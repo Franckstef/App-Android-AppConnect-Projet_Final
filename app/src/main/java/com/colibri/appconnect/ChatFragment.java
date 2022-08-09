2bae716b31066c0b011dc52b882cea1717f6f2e3
Kammill <47195728+Kammill@users.noreply.github.com>
2022-08-08 20:59:36 -0400

    [CL]: Add ConversationsFragment

--- app/src/main/java/com/colibri/appconnect/ChatFragment.java
+++ app/src/main/java/com/colibri/appconnect/ChatFragment.java
@@ -5,9 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import com.colibri.appconnect.databinding.FragmentChatBinding;
+
 public class ChatFragment extends Fragment {
+
+    private static final String TAG = "ChatFragment";
+    FragmentChatBinding binding;
 
     public ChatFragment() {
         // Required empty public constructor
@@ -18,12 +24,42 @@ public class ChatFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    //        FirebaseFirestore.getInstance().collection("users").orderBy("attrib").limit(1)
+    //        .addSnapshotListener((value, e) -> {
+    //            if (e != null) {
+    //                Log.w(TAG, "listen:error", e);
+    //                return;
+    //            }
+    //
+    //            assert value != null;
+    //            for (DocumentChange dc : value.getDocumentChanges()) {
+    //                switch (dc.getType()) {
+    //                    case ADDED:
+    //                        Log.d(TAG, "New city: " + dc.getDocument().getData());
+    //                        break;
+    //                    case MODIFIED:
+    //                        Log.d(TAG, "Modified city: " + dc.getDocument().getData());
+    //                        break;
+    //                    case REMOVED:
+    //                        Log.d(TAG, "Removed city: " + dc.getDocument().getData());
+    //                        break;
+    //                }
+    //            }
+    //        });
+
+
+
     }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_chat, container, false);
+        return binding.getRoot();
     }
 }
---
+++ app/src/main/java/com/colibri/appconnect/ConversationsFragment.java
@@ -0,0 +1,28 @@
+package com.colibri.appconnect;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class ConversationsFragment extends Fragment {
+
+    public ConversationsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_conversations, container, false);
+    }
+}
