641e83ad18dae008dd59451d8647eb97e54fbbf4
Kammill <47195728+Kammill@users.noreply.github.com>
2022-08-09 21:54:28 -0400

    [CL]: Add authentication and databinding in MainActivity

--- app/src/main/java/com/colibri/appconnect/MainActivity.java
+++ app/src/main/java/com/colibri/appconnect/MainActivity.java
@@ -1,25 +1,71 @@
 package com.colibri.appconnect;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.colibri.appconnect.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+
 
         Button connexion = findViewById(R.id.buttonConnexion);
 
         connexion.setOnClickListener(v -> {
-            Intent HomeIntent = new Intent(getApplicationContext(),HomeActivity.class);
-            startActivity(HomeIntent);
+
+            mAuth.signInWithEmailAndPassword(binding.editTextPseudo.getText().toString(), binding.editTextPassword.getText().toString())
+                    .addOnCompleteListener(this, task -> {
+
+                        if (task.isSuccessful()) {
+                            Intent HomeIntent = new Intent(getApplicationContext(),HomeActivity.class);
+                            startActivity(HomeIntent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            if (v != null) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+            }
         });
     }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(this, "authenticated already", Toast.LENGTH_SHORT).show();
+
+            Intent HomeIntent = new Intent(getApplicationContext(),HomeActivity.class);
+            startActivity(HomeIntent);
+
+        }
+    }
 
 }
