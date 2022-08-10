d13e2103d247688b4bea408f622a1d71655a392e
Philippe Allard-Rousse <philippe.allard-rousse.1@collegeahuntsic.qc.ca>
2022-08-10 14:44:58 -0400

    AC-72: More fonctionnalité

--- app/src/main/java/com/colibri/appconnect/userprofile/SharedUserProfileViewModel.java
+++ app/src/main/java/com/colibri/appconnect/userprofile/SharedUserProfileViewModel.java
@@ -1,24 +1,45 @@
 package com.colibri.appconnect.userprofile;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.colibri.appconnect.util.Resource;
+
 public class SharedUserProfileViewModel extends ViewModel {
 
-    public SharedUserProfileViewModel(){
+
+    private final String userId;
+    private final String placeholderRepo;
+    private final MutableLiveData<Resource<UserProfile>> userProfile = new MutableLiveData<>(new Resource.Loading<>());
+
+    public SharedUserProfileViewModel(String placeholderRepo, String userId)
+    {
         super();
+        this.userId = userId;
+        this.placeholderRepo = placeholderRepo;
+        userProfile.postValue(new Resource.Success<>(UserProfile.MockUserProfile()));
+    }
+
+    public LiveData<Resource<UserProfile>> getUserProfile(){
+        return userProfile;
     }
 
     public static class Factory extends ViewModelProvider.NewInstanceFactory{
-        public Factory(){
+        private final String mPlaceholderRepo;
+        private final String mUserId;
 
+        public Factory(String placeholderRepo, String userId){
+            mPlaceholderRepo = placeholderRepo;
+            mUserId = userId;
         }
         @SuppressWarnings("unchecked")
         @Override
         @NonNull
         public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-            return (T) new SharedUserProfileViewModel();
+            return (T) new SharedUserProfileViewModel(mPlaceholderRepo, mUserId);
         }
     }
 }
