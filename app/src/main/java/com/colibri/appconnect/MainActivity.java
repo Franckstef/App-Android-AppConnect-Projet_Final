6ffe2135e034a1646e85e3955c8b8022ab687e09
Philippe Allard-Rousse <philippe.allard-rousse.1@collegeahuntsic.qc.ca>
2022-08-10 23:40:03 -0400

    Integrate AuthUi after 5 empty attemp

--- app/src/main/java/com/colibri/appconnect/MainActivity.java
+++ app/src/main/java/com/colibri/appconnect/MainActivity.java
@@ -15,11 +15,20 @@ import com.colibri.appconnect.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     ActivityMainBinding binding;
-
+    private int MissingInfoAttemp = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +50,13 @@ public class MainActivity extends AppCompatActivity {
         String email =   binding.editTextPseudo.getText().toString();
         String password = binding.editTextPassword.getText().toString();
         if (email == null || password == null || email.isEmpty() || password.isEmpty()) {
+            MissingInfoAttemp++;
+            if(MissingInfoAttemp < 5) {
                 Toast.makeText(this, R.string.Auth_InfoMissing, Toast.LENGTH_LONG).show();
+            }
+            else {
+                SignWithAuthUi();
+            }
             return;
         }
 
@@ -81,6 +96,42 @@ public class MainActivity extends AppCompatActivity {
     private void StartHomeActivity(){
         Intent HomeIntent = new Intent(getApplicationContext(),HomeActivity.class);
         startActivity(HomeIntent);
+    }
+
+    private void SignWithAuthUi(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.logo)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            StartHomeActivity();
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
     }
 
     private static final String TAG = "MainActivity";
