6791045d11423af42b7c57d18029b333e6011cc0
Franck Stefani <franckstefani1@gmail.com>
2022-08-15 17:31:05 -0400

    [FS] UI fragment menu

--- app/src/main/java/com/colibri/appconnect/MenuFragment.java
+++ app/src/main/java/com/colibri/appconnect/MenuFragment.java
@@ -1,64 +1,61 @@
 package com.colibri.appconnect;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MenuFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class MenuFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    View view;
 
     public MenuFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MenuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MenuFragment newInstance(String param1, String param2) {
-        MenuFragment fragment = new MenuFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        view = inflater.inflate(R.layout.fragment_menu, container, false);
+        GridView menu = view.findViewById(R.id.gridMenu);
+
+        ArrayList<MenuItem> list = new ArrayList<>();
+        list.add(new MenuItem("Histoire", R.drawable.ic_histoire));
+        list.add(new MenuItem("Ressources", R.drawable.ic_ressources24));
+        list.add(new MenuItem("Formulaires", R.drawable.ic_document));
+        list.add(new MenuItem("Evenements", R.drawable.ic_event));
+        list.add(new MenuItem("Réglages", R.drawable.ic_settings));
+        list.add(new MenuItem("FAQ", R.drawable.ic_question));
+
+        MenuItemAdapter adapter = new MenuItemAdapter(getContext(), list);
+        menu.setAdapter(adapter);
+
+        menu.setOnItemClickListener((parent, view, position, id) -> {
+            switch (position) {
+                case 0:
+                    Intent intent = new Intent(getContext(), MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case 1:
+                    Intent intent4 = new Intent(getContext(), HomeActivity.class);
+                    startActivity(intent4);
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        return view;
     }
+
 }
